cb1168c859253ba1435bb96b6ad5adce61a5cfb9
Roman <bekkerrdm@gmail.com>
2021-01-15 20:05:46 +0700

    water v 0.2

--- data/test-water.py
+++ data/test-water.py
@@ -9,18 +9,19 @@ from pygame import *
 
 
 class SurfaceWater:
-    k = 0.025  # spring constant
-    d = 0.025  # damping constant
+    k = 0.02  # коэф пружины
+    d = 0.025  # коэф
 
     def __init__(self, x, y):
         self.x_pos = x
         self.y_pos = y
-        self.max_y = y
+        self.max_y = y  # естественное положение верхней части пружины
         self.velocity = 0
 
     def update(self):
         x = self.y_pos - self.max_y  # смещение пружины от начала
-        a = -self.k * x  # ускорение
+
+        a = -self.k * x - self.d * self.velocity  # ускорение
 
         self.y_pos += self.velocity
         self.velocity += a
@@ -34,8 +35,8 @@ class Water:
         self.x_e = x_e
         self.y_e = y_e
 
-        self.passes = 10
-        self.spread = 0.05  # скорость распространения волн
+        self.passes = 20
+        self.spread = 0.06  # скорость распространения волн
 
         for i in range(abs(x_e - x_s) // spring_segment):
             self.springs.append(SurfaceWater(i * spring_segment + x_s, y_e))
@@ -48,18 +49,19 @@ class Water:
         left_d = [0.0] * len(self.springs)
         right_d = [0.0] * len(self.springs)
 
-        for i in range(self.passes):
+        for _ in range(self.passes):
             for num in range(len(self.springs) - 1):
                 if num > 0:
-                    left_d[num] = self.spread * (self.springs[i].y_pos - self.springs[num - 1].y_pos)
+                    left_d[num] = self.spread * (self.springs[num].y_pos - self.springs[num - 1].y_pos)
                     self.springs[num - 1].velocity += left_d[num]
                 if num < len(self.springs) - 1:
-                    right_d[num] = self.spread * (self.springs[i].y_pos - self.springs[num + 1].y_pos)
+                    right_d[num] = self.spread * (self.springs[num].y_pos - self.springs[num + 1].y_pos)
                     self.springs[num + 1].velocity += right_d[num]
+
             for num in range(len(self.springs) - 1):
-                if i > 0:
+                if num > 0:
                     self.springs[num - 1].y_pos += left_d[num]  # you were updating velocity here!
-                if i < len(self.springs) - 1:
+                if num < len(self.springs) - 1:
                     self.springs[num + 1].y_pos += right_d[num]
 
     def draw(self):
@@ -77,11 +79,7 @@ if __name__ == '__main__':
     screen = pygame.display.set_mode(size)
     clock = pygame.time.Clock()
 
-    k = 0.025  # adjust this value to your liking
-    x = 250  # естественное положение верхней части пружины
-    d = 0.025  # коэф увлажнения
-
-    water = Water(0, 150, w, 150, 3)
+    water = Water(0, 150, w, 150, 4)
 
     run = True
     while run:
@@ -92,7 +90,8 @@ if __name__ == '__main__':
             if event.type == QUIT:
                 run = False
             if event.type == MOUSEBUTTONDOWN:
-                water.force(50, 100)
+                print(event.pos, event.pos[0])
+                water.force(event.pos[0], 100)
 
         pygame.display.flip()
 
