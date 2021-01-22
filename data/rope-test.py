97a5ec79af2b913d063ebfe0641da68827bdb84d
Roman <66955831+chibiherbie@users.noreply.github.com>
2021-01-23 02:00:52 +0700

    move Rope
    -свойства верёвки

--- data/rope-test.py
+++ data/rope-test.py
@@ -5,7 +5,7 @@ from pygame.locals import *
 
 
 r = [[0.0, 0.0], [0.0, -1.0], [0.0, -2.0], [0.0, -3.0], [0.0, -5.0]]
-conect = [[0, 1], [1, 2], [2, 3], [3, 4]]
+connect = [[0, 1], [1, 2], [2, 3], [3, 4]]
 
 
 class Rope:
@@ -17,11 +17,11 @@ class Rope:
 
         self.scale = 50
 
-        for stick in conect:
+        for stick in connect:
             self.sticks.append([stick[0], stick[1],
                                 self.get_distance(self.points[stick[0]][:2], self.points[stick[1]][:2])])
 
-        # размещаем на позиции
+        # размещаем на позицию
         point = self.points[0]
         point[0] = self.orig_points[0][0] + 100 / self.scale
         point[1] = self.orig_points[0][1] + 100 / self.scale
@@ -32,11 +32,32 @@ class Rope:
     def get_distance(self, p1, p2):
         return sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)  # находим гипотенузу
 
-    def add_stick(self, p):
-        pass
+    def upd_sticks(self):
+        for stick in self.sticks:
+            d = self.get_distance(self.points[stick[0]][:2], self.points[stick[1]][:2])
+            mv_ratio = (stick[2] - d) / d / 2  # разница в дистанции
+
+            # положение
+            dx = self.points[stick[1]][0] - self.points[stick[0]][0]
+            dy = self.points[stick[1]][1] - self.points[stick[0]][1]
+
+            if stick[0] != 0:
+                self.points[stick[0]][0] -= dx * mv_ratio * 0.85
+                self.points[stick[0]][1] -= dy * mv_ratio * 0.85
+
+            self.points[stick[1]][0] += dx * mv_ratio * 0.85
+            self.points[stick[1]][1] += dy * mv_ratio * 0.85
 
     def update(self):
-        pass
+        for i, point in enumerate(self.points):
+            if i != 0:
+                d_x = point[0] - point[2]
+                d_y = point[1] - point[3]
+                point[2] = point[0]
+                point[3] = point[1]
+                point[0] += d_x
+                point[1] += d_y
+                point[1] += 0.05  # придаём массу, всгда тянем вниз
 
     def draw(self, screen, color):
         x_points = [i[0] * self.scale for i in self.points]
@@ -46,12 +67,12 @@ class Rope:
         surf = pygame.Surface(screen.get_size())
         surf.set_colorkey((0, 0, 0))
 
-        # render points
+        # рисуем points
         render_points = [[i[0] * self.scale - int(min_x), i[1] * self.scale - int(min_y)] for i in self.points]
         for stick in self.sticks:
-            pygame.draw.line(surf, (255, 255, 255), render_points[stick[0]], render_points[stick[1]], 5)
+            pygame.draw.line(surf, color, render_points[stick[0]], render_points[stick[1]], 5)
 
-        screen.blit(surf, (100, 100))
+        screen.blit(surf, (min_x, min_y))
 
 
 pygame.init()
@@ -63,6 +84,10 @@ rope = Rope()
 
 while True:
     screen.fill((0, 0, 0))
+
+    # process
+    rope.update()
+    rope.upd_sticks()
 
     rope.draw(screen, (255, 255, 255))
 
